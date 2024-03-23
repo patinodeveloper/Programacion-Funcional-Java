66f4e2fd10d8607c1dbaa8d00df298f3ca45cbb8
patinodeveloper <pepe_toni_02@hotmail.com>
2024-03-22 22:07:35 -0600

    Video 10 - Encadenando Operaciones de Flujo - Curso de Programacion Funcional en Java

--- src/flujo/Main.java
+++ src/flujo/Main.java
@@ -15,64 +15,89 @@ import flujo.interfaces.OperadorUnario;
 public class Main {
 
     public Main() {
-        // proveer().filtrar().transformar().actuar().reducir()
-        // reducir(actuar(transformar(filtrar(proveer()))))
-        
-        
-        
-        // Crear lista de enteros
-        //List<Integer> numeros = SuperFunciones.proveer(10, new Naturales());
-        Flujo<Integer> numeros = Flujo.proveer(10, new Proveedor<>() {
+//      Integer total = new Flujo(List.of(1, 2, 3, 4, 5, 6, 7, 8))
+        Integer total = 
+                Flujo.proveer(10, new Proveedor<Integer>() {
             Random random = new Random();
-
             @Override
             public Integer obtener() {
                 return random.nextInt(10);
             }
-        });
-        System.out.println(numeros);
-        // Obtener solo los numeros pares
-        Flujo<Integer> filtrados = numeros.filtrar(new Predicado<>() {
+        }).filtrar(new Predicado<>() {
             @Override
             public boolean test(Integer valor) {
                 return valor % 2 == 0;
             }
-        });
-        System.out.println(filtrados);
-        // Obtener el cuadrado de cada numero
-        //List<Integer> cuadrados = SuperFunciones.transformar(filtrados, new AlCuadrado());
-        Flujo<Integer> transformados = filtrados.transformar(new OperadorUnario<>() {
+        }).transformar(new OperadorUnario<>() {
             @Override
             public Integer aplicar(Integer valor) {
                 return valor * valor;
             }
-        });
-        System.out.println(transformados);
-        // Obtener cada numero convertido en cadena
-        Flujo<String> convertidosEnCadena = filtrados.transformar(new Funcion<Integer, String>() {
-            @Override
-            public String aplicar(Integer valor) {
-                return "Valor: " + valor;
-            }
-        });
-        System.out.println(convertidosEnCadena);
-        Consumidor<Integer> impresor = new Consumidor<>() {
+        }).actuar(new Consumidor<>() {
             @Override
             public void aceptar(Integer valor) {
                 System.out.println(valor);
             }
-        };
-        Flujo<Integer> actuados = transformados.actuar(impresor);
-        // Mostrar cada cuadrado por pantalla y no retornar nada
-        transformados.consumir(impresor);
-        // Mostrar una suma total de la lista que se mostró
-        //int total = SuperFunciones.reducir(actuados, 0, new Sumador());
-        Integer total = actuados.reducir(0, new OperadorBinario<>() {
+        }).reducir(0, new OperadorBinario<>() {
             @Override
             public Integer aplicar(Integer valor1, Integer valor2) {
                 return valor1 + valor2;
             }
         });
+        // SE PUEDE BORRAR
+//        // Crear lista de enteros
+//        //List<Integer> numeros = SuperFunciones.proveer(10, new Naturales());
+//        Flujo<Integer> numeros = Flujo.proveer(10, new Proveedor<>() {
+//            Random random = new Random();
+//
+//            @Override
+//            public Integer obtener() {
+//                return random.nextInt(10);
+//            }
+//        });
+//        System.out.println(numeros);
+//        // Obtener solo los numeros pares
+//        Flujo<Integer> filtrados = numeros.filtrar(new Predicado<>() {
+//            @Override
+//            public boolean test(Integer valor) {
+//                return valor % 2 == 0;
+//            }
+//        });
+//        System.out.println(filtrados);
+//        // Obtener el cuadrado de cada numero
+//        //List<Integer> cuadrados = SuperFunciones.transformar(filtrados, new AlCuadrado());
+//        Flujo<Integer> transformados = filtrados.transformar(new OperadorUnario<>() {
+//            @Override
+//            public Integer aplicar(Integer valor) {
+//                return valor * valor;
+//            }
+//        });
+//        System.out.println(transformados);
+//        // Obtener cada numero convertido en cadena
+//        Flujo<String> convertidosEnCadena = filtrados.transformar(new Funcion<Integer, String>() {
+//            @Override
+//            public String aplicar(Integer valor) {
+//                return "Valor: " + valor;
+//            }
+//        });
+//        System.out.println(convertidosEnCadena);
+//        Consumidor<Integer> impresor = new Consumidor<>() {
+//            @Override
+//            public void aceptar(Integer valor) {
+//                System.out.println(valor);
+//            }
+//        };
+//        Flujo<Integer> actuados = transformados.actuar(impresor);
+//        // Mostrar cada cuadrado por pantalla y no retornar nada
+//        transformados.consumir(impresor);
+//        // Mostrar una suma total de la lista que se mostró
+//        //int total = SuperFunciones.reducir(actuados, 0, new Sumador());
+//        Integer total = actuados.reducir(0, new OperadorBinario<>() {
+//            @Override
+//            public Integer aplicar(Integer valor1, Integer valor2) {
+//                return valor1 + valor2;
+//            }
+//        });
         System.out.println("Reduccion: " + total);
 
     }
